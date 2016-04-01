92d04d9cd44357cced0bf84c6931602649cfae01
Matthew Winder <matthew.winder>
2016-04-01 21:59:39 +0100

    Added install.ps1 script

---
+++ install.ps1
@@ -0,0 +1,16 @@
+
+function Main
+{
+    Install-Chocolatey
+    choco install git --notsilent
+
+    git clone https://github.com/mwinder/powershell-profile.git "Documents\WindowsPowershell"
+}
+
+function Install-Chocolatey
+{
+    (New-Object Net.WebClient).DownloadString("https://chocolatey.org/install.ps1") | iex
+    choco feature enable -name=allowGlobalConfirmation
+}
+
+Main
