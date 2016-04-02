bc568e886e8a00f0100df08f58e035d4bd54d3f8
Matthew Winder <@mwinder>
2016-04-02 11:37:19 +0100

    Sorted package installs alphabetically.

--- install.ps1
+++ install.ps1
@@ -14,17 +14,14 @@ function Install-Chocolatey
 
 function New-BaseSystem
 {
-    choco install googlechrome
-
     choco install 7zip
-    choco install sysinternals
-
-    choco install sublimetext3
-
+    choco install googlechrome
     choco install git --notsilent
     choco install gitextensions --notsilent
     choco install kdiff3 --notsilent
     choco install nodejs --notsilent
+    choco install sublimetext3
+    choco install sysinternals
     choco install winmerge --notsilent
     choco install winscp --notsilent
 }
