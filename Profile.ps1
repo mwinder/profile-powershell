b691e50838da5975ceb0057b9c76bb29a404f0b8
Matthew Winder <mwinder@users.noreply.github.com>
2019-07-11 22:35:46 +0100

    Add Azure CLI to user path

--- Profile.ps1
+++ Profile.ps1
@@ -15,8 +15,9 @@ function User-Path
     Include "$env:programfiles\Microsoft SQL Server\130\Tools\Binn" | `                 ## SQL Server
     Include "$env:programfiles\Microsoft VS Code" | `                                   ## Visual Studio Code
     Include "$env:programfiles\Sublime Text 3" | `                                      ## Sublime Text
-    Include "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin" | `
-    Include "C:\Program Files (x86)\Microsoft SDKs\F#\10.1\Framework\v4.0"
+    Include "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin" |`
+    Include "C:\Program Files (x86)\Microsoft SDKs\F#\10.1\Framework\v4.0"| `
+    Include "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 }
 
 function System-Path
